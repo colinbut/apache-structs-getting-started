6777dd66583ffcc3b6c890201b3df95a74bfc3da
Colin But <colinbut@users.noreply.github.com>
2017-10-09 23:33:37 +0100

    Initial maven generated project

---
+++ src/main/java/com/mycompany/apache/structs/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.apache.structs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
